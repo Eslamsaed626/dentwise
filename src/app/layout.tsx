ab08b1a4b708f6deff22051bce4e0cd33670abd3
Eslamsaed626 <eslamsaed626@gmail.com>
2025-12-28 07:28:47 +0300

    appointment page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import UserSync from "@/components/UserSync";
 import TanStackProvider from "@/components/providers/TanStackProvider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +45,7 @@ export default function RootLayout({
             className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
           >
             <UserSync></UserSync>
+            <Toaster></Toaster>
             {children}
           </body>
         </html>
--- src/hooks/use-doctors.ts
+++ src/hooks/use-doctors.ts
@@ -1,6 +1,11 @@
 "use client";
 
-import { createDoctor, getDoctors, updateDoctor } from "@/lib/actions/doctors";
+import {
+  createDoctor,
+  getAvailableDoctors,
+  getDoctors,
+  updateDoctor,
+} from "@/lib/actions/doctors";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export function useGetDoctors() {
@@ -35,6 +40,16 @@ export function useUpdateDoctor() {
       queryClient.invalidateQueries({ queryKey: ["getDoctors"] });
     },
     onError: (error) => console.log("error while updating a doctor"),
+  });
+
+  return result;
+}
+
+// get available doctors for appointments
+export function useAvailableDoctors() {
+  const result = useQuery({
+    queryKey: ["getAvailableDoctors"],
+    queryFn: getAvailableDoctors,
   });
 
   return result;
